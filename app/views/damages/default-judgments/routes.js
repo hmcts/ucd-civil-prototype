b307956cda08d70ac5c1c50c80ec3d54f69eaceb
monicasara <monica.santos@hmcts.net>
2021-10-13 17:02:36 +0100

    Multiple dates add possibility

--- app/views/damages/default-judgments/routes.js
+++ app/views/damages/default-judgments/routes.js
@@ -1,3 +1,5 @@
+const { dateCount } = require("../../../data/session-data-defaults");
+
 module.exports = (router) => {
 
 router.post('/damages/default-judgments/2-defendant', function(req, res) {
@@ -21,31 +23,6 @@ router.post('/damages/default-judgments/2-defendant', function(req, res) {
         res.redirect('/index')
     }    
 })
-
-router.post('/sps/work-in-progress/how-to-get-tax-letters', (req, res) => {
-	var errors = []
-	if (req.body['howContacted'] === undefined) {
-	  errors.push({
-		text: 'Select how you want to get your tax letters',
-		href: '#how-to-get-tax-letters'
-	  })
-	}
-	if (errors.length === 0) {
-	  if (req.body['howContacted'] === 'Online') {
-		res.redirect('/sps/work-in-progress/add-email')
-	  } 
-    else {
-        if (req.session.data.route === 'external') {
-          res.redirect('/sps/work-in-progress/confirmation-post-alternative')
-        }
-        else {
-        res.redirect('/sps/work-in-progress/confirmation-post')
-      }
-	  }
-	} else {
-	  res.render('.//sps/work-in-progress/how-toget-tax-letters', { errors: errors })
-	}
-  })
 
 
 router.post('/damages/default-judgments/3-request-judgment-statements', function(req, res) {
@@ -96,11 +73,34 @@ router.post('/damages/default-judgments/exclude-hearing-dates', function(req, re
   res.redirect('/damages/default-judgments/5-hearing-details')
 })
 
+// for page 5-hearing-details
+router.post('/damages/default-judgments/exclude-hearing-dates', function(req, res) {
+  dateCount = req.session.data.dateCount
+  req.session.data.dayFrom[dateCount] = req.body['date-from-day']
+  req.session.data.monthFrom[dateCount] = req.body['date-from-month']
+  req.session.data.yearFrom[dateCount] = req.body['date-from-year']
+  req.session.data.dayTo[dateCount] = req.body['date-to-day']
+  req.session.data.monthTo[dateCount] = req.body['date-to-month']
+  req.session.data.yearTo[dateCount] = req.body['date-to-year']
+
+  req.session.data.startDate[dateCount] = req.body['date-from-day'] + " " + req.session.data.shortMonthName[req.body['date-from-month']] + " " + req.body['date-from-year']
+  req.session.data.endDate[dateCount] = req.body['date-to-day'] + " " + req.session.data.shortMonthName[req.body['date-to-month']] + " " + req.body['date-to-year']
+
+  console.log("From", req.session.data.startDate[dateCount]);
+  console.log("To", req.session.data.endtDate[dateCount]);
+  console.log("dateCount", req.session.data.dateCount);
+
+  req.session.data.dateCount = dateCount + 1
+  console.log("dateCount", req.session.data.dateCount);
+
+  res.redirect('/damages/default-judgments/5-hearing-details')
+})
+
 router.post('/damages/default-judgments/5-hearing-details', function(req, res) {
   req.session.data.unavailable = req.body['exclusion-dates']
   if (req.body['submit-button'] === 'add') {
-    console.log("dateCount", req.session.data.dateCount)
-    dateCount = req.session.data.dateCount
+    count = req.session.data.dateCount
+    console.log("count start", count)
     req.session.data.dayFrom = req.body['date-from-day']
     req.session.data.monthFrom = req.body['date-from-month']
     req.session.data.yearFrom = req.body['date-from-year']
@@ -108,12 +108,14 @@ router.post('/damages/default-judgments/5-hearing-details', function(req, res) {
     req.session.data.monthTo = req.body['date-to-month']
     req.session.data.yearTo = req.body['date-to-year']
   
-    req.session.data.startDate = req.body['date-from-day'] + " " + req.session.data.shortMonthName[req.body['date-from-month']] + " " + req.body['date-from-year']
-    req.session.data.endDate = req.body['date-to-day'] + " " + req.session.data.shortMonthName[req.body['date-to-month']] + " " + req.body['date-to-year']
+    req.session.data.id[count] = count
+    req.session.data.startDate[count] = req.body['date-from-day'] + " " + req.session.data.shortMonthName[req.body['date-from-month']] + " " + req.body['date-from-year']
+    req.session.data.endDate[count] = req.body['date-to-day'] + " " + req.session.data.shortMonthName[req.body['date-to-month']] + " " + req.body['date-to-year']
   
+    console.log("From", req.session.data.startDate[count]);
     console.log("unavailable", req.session.data.unavailable)
   
-    req.session.data.dateCount = dateCount + 1
+    req.session.data.dateCount = count + 1
     console.log("dateCount", req.session.data.dateCount)
   
     res.redirect('/damages/default-judgments/5-hearing-details#unavailable-dates')
@@ -125,6 +127,36 @@ router.post('/damages/default-judgments/5-hearing-details', function(req, res) {
       res.redirect('/damages/default-judgments/4-make-request-judgment')
   }    
 })
+
+// router.post('/damages/default-judgments/5-hearing-details', function(req, res) {
+//   req.session.data.unavailable = req.body['exclusion-dates']
+//   if (req.body['submit-button'] === 'add') {
+//     console.log("dateCount", req.session.data.dateCount)
+//     dateCount = req.session.data.dateCount
+//     req.session.data.dayFrom = req.body['date-from-day']
+//     req.session.data.monthFrom = req.body['date-from-month']
+//     req.session.data.yearFrom = req.body['date-from-year']
+//     req.session.data.dayTo = req.body['date-to-day']
+//     req.session.data.monthTo = req.body['date-to-month']
+//     req.session.data.yearTo = req.body['date-to-year']
+  
+//     req.session.data.startDate = req.body['date-from-day'] + " " + req.session.data.shortMonthName[req.body['date-from-month']] + " " + req.body['date-from-year']
+//     req.session.data.endDate = req.body['date-to-day'] + " " + req.session.data.shortMonthName[req.body['date-to-month']] + " " + req.body['date-to-year']
+  
+//     console.log("unavailable", req.session.data.unavailable)
+  
+//     req.session.data.dateCount = dateCount + 1
+//     console.log("dateCount", req.session.data.dateCount)
+  
+//     res.redirect('/damages/default-judgments/5-hearing-details#unavailable-dates')
+//     }
+//   else if (req.body['submit-button'] === 'continue') {
+//     res.redirect('/damages/default-judgments/6-check-your-answers')
+//   }
+//   else {
+//       res.redirect('/damages/default-judgments/4-make-request-judgment')
+//   }    
+// })
 
 router.post('/damages/default-judgments/6-check-your-answers', function(req, res) {
   if (req.body['submit-button'] === 'continue') {
