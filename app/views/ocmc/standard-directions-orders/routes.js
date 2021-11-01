d6367e15a3099841e6ba74bf75f6e7bfa0ed2d6f
CJGov <75368642+CJGov@users.noreply.github.com>
2021-11-01 14:29:18 +0000

    updated SDO

--- app/views/ocmc/standard-directions-orders/routes.js
+++ app/views/ocmc/standard-directions-orders/routes.js
@@ -2,12 +2,29 @@ module.exports = (router) => {
 
 router.post('/ocmc/standard-directions-orders/1-track', function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/standard-directions-orders/2-something')
+    res.redirect('/ocmc/standard-directions-orders/2a-Generate-Directions-Order')
   }
   else {
       res.redirect('/index')
-  }    
+  }
 })
 
+router.post('/ocmc/standard-directions-orders/2a-Generate-Directions-Order', function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect('/ocmc/standard-directions-orders/3-open-directions')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/1-track')
+  }
+})
+
+router.post('/ocmc/standard-directions-orders/3-open-directions', function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect('/ocmc/standard-directions-orders/4-confirmation')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/2a-Generate-Directions-Order')
+  }
+})
 
 };
