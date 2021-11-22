a52684a5d6dc125c7363f5458d2fb7f1c346020e
Curtis John <75368642+CJGov@users.noreply.github.com>
2021-11-22 09:22:06 +0000

    updates

--- app/views/ocmc/general-applications/routes.js
+++ app/views/ocmc/general-applications/routes.js
@@ -2,16 +2,16 @@ module.exports = (router) => {
 
 router.post('/ocmc/general-applications/application-submission/1-application', function(req, res) {
   if (req.body['submit-button'] === 'add') {
-    
+
     count = req.session.data.applicationCount
     console.log("count start", count)
 
     req.session.data.application[count] = req.body['order']
     req.session.data.idApplication[count] = count
-  
+
     req.session.data.applicationCount = count + 1
     console.log("applicationCount", req.session.data.applicationCount)
-  
+
     res.redirect('/ocmc/general-applications/application-submission/1-application')
     }
   else if (req.body['submit-button'] === 'continue') {
@@ -19,7 +19,7 @@ router.post('/ocmc/general-applications/application-submission/1-application', f
   }
   else {
       res.redirect('/index')
-  }    
+  }
 })
 
 router.post('/ocmc/general-applications/application-submission/2-respondent-agreement', function(req, res) {
@@ -28,7 +28,7 @@ router.post('/ocmc/general-applications/application-submission/2-respondent-agre
   }
   else {
       res.redirect('/ocmc/general-applications/application-submission/1-application')
-  }    
+  }
 })
 
 router.post('/ocmc/general-applications/application-submission/3-urgent', function(req, res) {
@@ -37,8 +37,27 @@ router.post('/ocmc/general-applications/application-submission/3-urgent', functi
   }
   else {
       res.redirect('/ocmc/general-applications/application-submission/2-respondent-agreement')
-  }    
+  }
 })
 
+//Responce journey
+
+router.post('/ocmc/general-applications/Responce-journey/1-respond', function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect('/ocmc/general-applications/Responce-journey/2-hearing-details')
+  }
+})
+
+router.post('/ocmc/general-applications/Responce-journey/2-hearing-details', function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect('/ocmc/general-applications/Responce-journey/3-check-your-answers')
+  }
+})
+
+router.post('/ocmc/general-applications/Responce-journey/3-check-your-answers', function(req, res) {
+    if (req.body['submit-button'] === 'continue') {
+        res.redirect('/ocmc/general-applications/Responce-journey/4-confirmation')
+    }
+})
 
 };
