d15d31abb5ab7dccd499cca2282acefdc1396196
ZedAliHMCTS <zahid.ali@hmcts.net>
2021-11-29 15:51:18 +0000

    WIP - Z

--- app/views/ocmc/general-applications/routes.js
+++ app/views/ocmc/general-applications/routes.js
@@ -1,6 +1,9 @@
 module.exports = (router) => {
 
-router.post('/ocmc/general-applications/application-submission/1-application', function(req, res) {
+const SUBMISSION_URI = '/ocmc/general-applications/application-submission'
+const VIEWS_PATH = 'ocmc/general-applications/application-submission'
+
+router.post(`${SUBMISSION_URI}/1-application`, function(req, res) {
   if (req.body['submit-button'] === 'add') {
 
     count = req.session.data.applicationCount
@@ -12,76 +15,98 @@ router.post('/ocmc/general-applications/application-submission/1-application', f
     req.session.data.applicationCount = count + 1
     console.log("applicationCount", req.session.data.applicationCount)
 
-    res.redirect('/ocmc/general-applications/application-submission/1-application')
+    res.redirect(`${SUBMISSION_URI}/1-application`)
     }
   else if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/2-respondent-agreement')
+    res.redirect(`${SUBMISSION_URI}/2-respondent-agreement`)
   }
   else {
       res.redirect('/index')
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/2-respondent-agreement', function(req, res) {
+router.post(`${SUBMISSION_URI}/2-respondent-agreement`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/3-urgent')
+    res.redirect(`${SUBMISSION_URI}/3-urgent`)
   }
   else {
-      res.redirect('/ocmc/general-applications/application-submission/1-application')
+      res.redirect(`${SUBMISSION_URI}/1-application`)
   }
 })
 
 
 //respondant agreed branching space
-
-
-
-router.post('/ocmc/general-applications/application-submission/4-inform-other-party', function(req, res) {
+router.post(`${SUBMISSION_URI}/3-urgent`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/5-enter-order')
+    if(req.session.data['agreement'] && req.session.data['agreement'] == 'no')
+      res.redirect(`${SUBMISSION_URI}/4-inform-other-party`)
+    else
+      res.redirect(`${SUBMISSION_URI}/5-enter-order`)
   }
   else {
-      res.redirect('/ocmc/general-applications/application-submission/5-enter-order')
+      res.redirect(`${SUBMISSION_URI}/1-application`)
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/4a-inform-other-party', function(req, res) {
+
+
+router.post(`${SUBMISSION_URI}/4-inform-other-party`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/5-enter-order')
+    res.redirect(`${SUBMISSION_URI}/5-enter-order`)
   }
   else {
-      res.redirect('/ocmc/general-applications/application-submission/5a-enter-order')
+      res.redirect(`${SUBMISSION_URI}/5-enter-order`)
+  }
+})
+
+router.post(`${SUBMISSION_URI}/4a-inform-other-party`, function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect(`${SUBMISSION_URI}/5-enter-order`)
+  }
+  else {
+      res.redirect(`${SUBMISSION_URI}/5a-enter-order`)
   }
 })
 
 
-router.post('/ocmc/general-applications/application-submission/5-enter-order', function(req, res) {
+
+router.get(`${SUBMISSION_URI}/5-enter-order`, function(req, res) {
+  var obj = { showUpload: true, showSoT: true }
+  if (req.session.data['agreement'] == 'yes'){
+    obj.showUpload = false;
+    obj.showSoT = false;
+  }
+
+  res.render(`${VIEWS_PATH}/5-enter-order`, obj);
+})
+
+router.post(`${SUBMISSION_URI}/5-enter-order`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/6-hearing-details')
+    res.redirect(`${SUBMISSION_URI}/6-hearing-details`)
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/5a-enter-order', function(req, res) {
+router.post(`${SUBMISSION_URI}/5a-enter-order`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/6-hearing-details')
+    res.redirect(`${SUBMISSION_URI}/6-hearing-details`)
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/6-hearing-details', function(req, res) {
+router.post(`${SUBMISSION_URI}/6-hearing-details`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/7-account-number')
+    res.redirect(`${SUBMISSION_URI}/7-account-number`)
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/7-account-number', function(req, res) {
+router.post(`${SUBMISSION_URI}/7-account-number`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/8-check-answers')
+    res.redirect(`${SUBMISSION_URI}/8-check-answers`)
   }
 })
 
-router.post('/ocmc/general-applications/application-submission/8-check-answers', function(req, res) {
+router.post(`${SUBMISSION_URI}/8-check-answers`, function(req, res) {
   if (req.body['submit-button'] === 'continue') {
-    res.redirect('/ocmc/general-applications/application-submission/9-confirmation')
+    res.redirect(`${SUBMISSION_URI}/9-confirmation`)
   }
 })
 
@@ -110,3 +135,4 @@ router.post('/ocmc/general-applications/Responce-journey/3-check-answers', funct
 //judge journey
 
 };
+
