1bc6174212746da85924dcd4268e3197d6d215b8
CJGov <75368642+CJGov@users.noreply.github.com>
2022-02-11 14:46:34 +0000

    SDO updated

--- app/views/ocmc/standard-directions-orders/routes.js
+++ app/views/ocmc/standard-directions-orders/routes.js
@@ -9,6 +9,58 @@ router.post('/ocmc/standard-directions-orders/1-track', function(req, res) {
   }
 })
 
+router.post('/ocmc/standard-directions-orders/1-enter-judgment', function(req, res) {
+  if (req.body['damages'] === 'true') {
+    res.redirect('/ocmc/standard-directions-orders/allocate-small-claims')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/2-track-selection')
+  }
+})
+
+//fast track
+
+router.post('/ocmc/standard-directions-orders/2-track-selection', function(req, res) {
+  if (req.body['claims-track'] === 'Small-claims-track') {
+    res.redirect('/ocmc/standard-directions-orders/order-details-small-claims')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/order-details-fast-track')
+  }
+})
+
+//small claims
+
+router.post('/ocmc/standard-directions-orders/Allocate-small-claims', function(req, res) {
+  if (req.body['small-track'] === 'true') {
+    res.redirect('/ocmc/standard-directions-orders/order-details-small-claims')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/order-type')
+  }
+})
+
+
+router.post('/ocmc/standard-directions-orders/order-type', function(req, res) {
+  if (req.body['Disposal'] === 'true') {
+    res.redirect('/ocmc/standard-directions-orders/disposal-hearing-SDO')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/order-details-fast-track')
+  }
+})
+
+router.post('/ocmc/standard-directions-orders/disposal-hearing-SDO', function(req, res) {
+  if (req.body['submit-button'] === 'continue') {
+    res.redirect('/ocmc/standard-directions-orders/3-open-directions')
+  }
+  else {
+      res.redirect('/ocmc/standard-directions-orders/order-type')
+  }
+})
+
+//Directions
+
 router.post('/ocmc/standard-directions-orders/2a-Generate-Directions-Order', function(req, res) {
   if (req.body['submit-button'] === 'continue') {
     res.redirect('/ocmc/standard-directions-orders/3-open-directions')
