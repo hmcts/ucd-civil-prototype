c669507d51ec82508900c66c19233c77439687c1
CJGov <75368642+CJGov@users.noreply.github.com>
2022-02-16 11:07:11 +0000

    back button

--- app/views/ocmc/standard-directions-orders/routes.js
+++ app/views/ocmc/standard-directions-orders/routes.js
@@ -65,9 +65,7 @@ router.post('/ocmc/standard-directions-orders/2a-Generate-Directions-Order', fun
   if (req.body['submit-button'] === 'continue') {
     res.redirect('/ocmc/standard-directions-orders/3-open-directions')
   }
-  else {
-      res.redirect('/ocmc/standard-directions-orders/1-track')
-  }
+
 })
 
 router.post('/ocmc/standard-directions-orders/3-open-directions', function(req, res) {
@@ -75,7 +73,7 @@ router.post('/ocmc/standard-directions-orders/3-open-directions', function(req, 
     res.redirect('/ocmc/standard-directions-orders/4-confirmation')
   }
   else {
-      res.redirect('/ocmc/standard-directions-orders/2a-Generate-Directions-Order')
+      res.redirect('javascript:window.history.back()')
   }
 })
 
